1a49ef23ed540d4f518d920c8245154768f460e8
Kevin Carter <console.logkevin@gmail.com>
2024-03-07 08:39:46 -0500

    bring in dog dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kvncrtr/puppy
 
 go 1.22.0
+
+require github.com/kvncrtr/dog v0.0.0-20240307133052-538cabb069b4
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/kvncrtr/dog"
+)
+
 func Bark() string {
 	return "Woof"
 }
 
 func Barks() string {
 	return "WOOF! WOOF! WOOF!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
